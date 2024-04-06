6aecf9c0eeba21417f7166862df14c632429d6ba
mulimuoki001 <b.muoki@alustudent.com>
2024-04-07 01:51:49 +0200

    updated files

--- FarmerBora_App/static/forum.js
+++ FarmerBora_App/static/forum.js
@@ -17,8 +17,18 @@ function showIconBar() {
 function showComment() {
     var commentArea = document.getElementById("comment-area");
     commentArea.classList.remove("hide");
+
+}
+function hideComment() {
+    var commentArea = document.getElementById("comment-area");
+    commentArea.classList.add("hide");
 }
 function showReply() {
     var replyArea = document.getElementById("reply-area");
     replyArea.classList.remove("hide");
+
+}
+function hideReply() {
+    var commentArea = document.getElementById("reply-area");
+    commentArea.classList.add("hide");
 }
