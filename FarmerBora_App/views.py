91cda0e2c36993357c9f5566f8be31063fba1a93
mulimuoki001 <b.muoki@alustudent.com>
2024-04-09 22:26:06 +0200

    updated files

--- FarmerBora_App/views.py
+++ FarmerBora_App/views.py
@@ -77,21 +77,6 @@ def watchvideo(request):
 
 def details(request, slug):
     post = get_object_or_404(Post, slug=slug)
-    author = Author.objects.get(user=request.user)
-
-    if request.method == "POST":
-        if "comment-form" in request.POST:
-            comment = request.POST.get("comment")
-            new_comment, created = Comment.objects.get_or_create(
-                user=author, content=comment, post=post
-            )
-            post.comments.add(new_comment)
-        elif "reply-form" in request.POST:
-            reply = request.POST.get("reply")
-            comment_id = request.POST.get("comment-id")
-            comment_obj = Comment.objects.get(id=comment_id)
-            new_reply, created = Reply.objects.get_or_create(user=author, content=reply)
-            comment_obj.replies.add(new_reply)
 
     context = {
         "post": post,
